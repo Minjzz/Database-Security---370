c7e0d70f779405d01b05ef76ae5b97a813c532f0
Marlon Villegas <marlonvillegas00@gmail.com>
2026-02-21 10:27:27 +0800

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'dbname' => 'secure_app',
+    'username' => 'secure_user',
+    'password' => 'strong_password_here',
+    'charset' => 'utf8mb4'
+];
